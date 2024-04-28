4a58ea9f168202330087ccdbcfe18889eaf92346
BlazeGAI <165929771+BlazeGAI@users.noreply.github.com>
2024-04-28 11:54:56 -0400

    Update 4_📞_Contact.py

--- pages/4_📞_Contact.py
+++ pages/4_📞_Contact.py
@@ -11,13 +11,7 @@ st.write(
 )
 st.sidebar.image("https://i.imgur.com/PD23Zwd.png", width=250)
 
-tawkto_iframe_html = """
-<iframe src="https://tawk.to/chat/6594827c0ff6374032bb6e17/1hj61rdoe" 
-        height="500" 
-        width="682" 
-        frameborder="0">
-</iframe>
-"""
+
 # Embed the iframe in the Streamlit app
 components.html(tawkto_iframe_html, height=520)
 
