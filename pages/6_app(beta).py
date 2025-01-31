85261f2e0367cf346797798787568fbb5095fbaf
BlazeGAI <165929771+BlazeGAI@users.noreply.github.com>
2025-01-31 13:33:58 -0500

    Update 6_app(beta).py

--- pages/6_app(beta).py
+++ pages/6_app(beta).py
@@ -117,10 +117,6 @@ def main():
             "Week 1": "1",
             "Week 2": "2",
             "Week 3": "3",
-            "Week 4": "4",
-            "Week 5": "5",
-            "Week 6": "6",
-            "Week 7": "7"
         }
 
         base_url = f"https://online.tiffin.edu/course/section.php?id={course_id}"
